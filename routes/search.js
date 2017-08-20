11cd11c2b4bb705f44176d4a667eec002267696e
wwSchrader <wwschrader@gmail.com>
2017-08-19 17:43:35 -0700

    Update formatting of code to conform to lint standards

--- routes/search.js
+++ routes/search.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var router = express.Router();
-var fetch = require('node-fetch');
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
 const yelp = require('yelp-fusion');
 
 
 /* GET yelp search listings. */
 router.get('/', function(req, res, next) {
-    yelp.accessToken(process.env.YELP_ID, process.env.YELP_SECRET).then(response => {
+  yelp.accessToken(process.env.YELP_ID, process.env.YELP_SECRET)
+    .then((response) => {
       const client = yelp.client(response.jsonBody.access_token);
 
       client.search({
-        term:'bars',
-        location: 'San Francisco, CA'
-      }).then(response => {
-        console.log("Received yelp results");
+        term: 'bars',
+        location: 'San Francisco, CA',
+      }).then((response) => {
+        console.log('Received yelp results');
         res.json(response.jsonBody.businesses);
       });
-    }).catch(e => {
+    }).catch((e) => {
       console.log(e);
     });
 });
-
 
 module.exports = router;
