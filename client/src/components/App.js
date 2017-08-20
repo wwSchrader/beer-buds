7d79b5e71cb6b87d402a342b8a62db357db03ddc
wwSchrader <wwschrader@gmail.com>
2017-08-20 15:44:14 -0700

    Reorganize the client folder

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Beer Buds</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
