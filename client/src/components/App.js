8396cc137dd0de2d3a16ddc751a4841e11e6b57a
wwSchrader <wwschrader@gmail.com>
2017-08-20 18:28:34 -0700

    Change search bar to controlled form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,17 +6,26 @@ import '../css/App.css';
  * Main App Component
  */
 class App extends Component {
-  /**
-   * Render UI
-   * @returns {JSX} returns jsx for app
-  */
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+
+    this.onSearchTermUpdate = this.onSearchTermUpdate.bind(this);
+  }
+
+  onSearchTermUpdate(newSearchTerm) {
+    this.setState({searchTerm: newSearchTerm});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Plans tonight?</h1>
         <h4>See which bars are hoppin' tonight and RSVP ahead of time!</h4>
         <h4>Remember: take a cab and drink responsibly.</h4>
-        <SearchForm />
+        <SearchForm searchTermHandler={this.onSearchTermUpdate} />
       </div>
     );
   }
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -5,15 +5,29 @@ import {Form, FormGroup, FormControl, Button} from 'react-bootstrap';
  * Display search bar and search button.
  */
 class SearchForm extends Component {
-  /**
-  * Render search bar and submit button
-  * @returns {JSX} JSX for search bar and button
-  */
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchBoxText: '',
+    };
+
+    this.onSearchTermTextChange = this.onSearchTermTextChange.bind(this);
+  }
+
+  onSearchTermTextChange(e) {
+    this.setState({searchBoxText: e.target.value});
+  }
+
   render() {
     return (
       <Form inline>
         <FormGroup controlId='searchInput'>
-          <FormControl type='text' placholder='Where you at?' />
+          <FormControl
+              type='text'
+              onChange={this.onSearchTermTextChange}
+              value={this.state.searchBoxText}
+              placeholder='Where you at?'
+          />
         </FormGroup>
         <Button type='submit'>
           Search
